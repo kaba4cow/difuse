5bbb008ab1d6350a38ab8eb5adf1d4d1d745e37a
kaba4cow <kaba4cow@gmail.com>
2025-07-04 15:00:50 +0300

    Added error handling to GlobalBeanSourceValidator

--- difuse-core/src/main/java/com/kaba4cow/difuse/core/bean/source/validator/support/GlobalBeanSourceValidator.java
+++ difuse-core/src/main/java/com/kaba4cow/difuse/core/bean/source/validator/support/GlobalBeanSourceValidator.java
@@ -5,6 +5,7 @@ import java.util.Set;
 import com.kaba4cow.difuse.core.annotation.application.CoreComponent;
 import com.kaba4cow.difuse.core.annotation.application.CoreDependency;
 import com.kaba4cow.difuse.core.bean.source.BeanSource;
+import com.kaba4cow.difuse.core.bean.source.validator.BeanSourceValidationException;
 import com.kaba4cow.difuse.core.bean.source.validator.BeanSourceValidator;
 
 @CoreComponent
@@ -14,9 +15,15 @@ public class GlobalBeanSourceValidator {
 	private BeanSourceValidatorRegistry beanSourceValidatorRegistry;
 
 	public void validate(BeanSource<?> beanSource) {
-		Set<BeanSourceValidator> validators = beanSourceValidatorRegistry.getValidators(beanSource);
-		for (BeanSourceValidator validator : validators)
-			validator.validate(beanSource);
+		try {
+			Set<BeanSourceValidator> validators = beanSourceValidatorRegistry.getValidators(beanSource);
+			for (BeanSourceValidator validator : validators)
+				validator.validate(beanSource);
+		} catch (BeanSourceValidationException exception) {
+			throw new BeanSourceValidationException(String.format("Invalid BeanSource: %s", beanSource), exception);
+		} catch (Exception exception) {
+			throw new BeanSourceValidationException(String.format("Could not validate BeanSource: %s", beanSource), exception);
+		}
 	}
 
 }
