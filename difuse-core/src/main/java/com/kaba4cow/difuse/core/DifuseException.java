b910e6d68f602b2655d5417dac54314c8f9f9794
kaba4cow <kaba4cow@gmail.com>
2025-07-04 15:21:06 +0300

    Removed unnecessary constructors from DifuseException

--- difuse-core/src/main/java/com/kaba4cow/difuse/core/DifuseException.java
+++ difuse-core/src/main/java/com/kaba4cow/difuse/core/DifuseException.java
@@ -4,14 +4,6 @@ public class DifuseException extends RuntimeException {
 
 	private static final long serialVersionUID = 1L;
 
-	public DifuseException() {
-		super();
-	}
-
-	public DifuseException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-		super(message, cause, enableSuppression, writableStackTrace);
-	}
-
 	public DifuseException(String message, Throwable cause) {
 		super(message, cause);
 	}
