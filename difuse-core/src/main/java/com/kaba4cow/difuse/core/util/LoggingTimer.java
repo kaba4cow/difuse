0ab31b7e2fb064a6b33f2536e31f560345226288
kaba4cow <kaba4cow@gmail.com>
2025-07-05 23:46:25 +0300

    Created LoggingTimer

---
+++ difuse-core/src/main/java/com/kaba4cow/difuse/core/util/LoggingTimer.java
@@ -0,0 +1,33 @@
+package com.kaba4cow.difuse.core.util;
+
+import org.slf4j.Logger;
+
+public class LoggingTimer implements AutoCloseable {
+
+	private final Logger log;
+
+	private final long startTimeMillis;
+
+	private long finishTimeMillis;
+
+	private boolean finished;
+
+	public LoggingTimer(Logger log, String message, Object... args) {
+		this.log = log;
+		this.startTimeMillis = System.currentTimeMillis();
+		this.finishTimeMillis = 0L;
+		this.finished = false;
+		this.log.info(message, args);
+	}
+
+	@Override
+	public void close() {
+		if (finished)
+			throw new IllegalStateException("Timer has already finished");
+		finished = true;
+		finishTimeMillis = System.currentTimeMillis();
+		long elapsedTimeMillis = finishTimeMillis - startTimeMillis;
+		log.info("Finished, process took {} ms", elapsedTimeMillis);
+	}
+
+}
