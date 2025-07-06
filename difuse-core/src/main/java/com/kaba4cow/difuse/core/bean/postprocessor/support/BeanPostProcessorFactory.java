acd5d60aee16469c59fd8fc8a308bd288da9a09a
kaba4cow <kaba4cow@gmail.com>
2025-07-06 13:11:35 +0300

    Added SystemBeanInjector to Bean[Pre&Post]ProcessorFactory

--- difuse-core/src/main/java/com/kaba4cow/difuse/core/bean/postprocessor/support/BeanPostProcessorFactory.java
+++ difuse-core/src/main/java/com/kaba4cow/difuse/core/bean/postprocessor/support/BeanPostProcessorFactory.java
@@ -3,17 +3,23 @@ package com.kaba4cow.difuse.core.bean.postprocessor.support;
 import java.lang.reflect.Constructor;
 
 import com.kaba4cow.difuse.core.DifuseException;
+import com.kaba4cow.difuse.core.annotation.dependency.Provided;
 import com.kaba4cow.difuse.core.annotation.system.SystemBean;
 import com.kaba4cow.difuse.core.bean.postprocessor.BeanPostProcessor;
+import com.kaba4cow.difuse.core.system.bean.SystemBeanInjector;
 import com.kaba4cow.difuse.core.util.reflections.ConstructorScanner;
 
 @SystemBean
 public class BeanPostProcessorFactory {
 
+	@Provided
+	private SystemBeanInjector beanInjector;
+
 	public BeanPostProcessor createBeanPostProcessor(Class<? extends BeanPostProcessor> type) {
 		try {
 			Constructor<?> constructor = ConstructorScanner.of(type).findNoArgsConstructor();
 			Object instance = constructor.newInstance();
+			beanInjector.injectDependencies(instance);
 			return type.cast(instance);
 		} catch (Exception exception) {
 			throw new DifuseException(String.format("Could not create BeanPostProcessor of type %s", type), exception);
--- difuse-core/src/main/java/com/kaba4cow/difuse/core/bean/preprocessor/support/BeanPreProcessorFactory.java
+++ difuse-core/src/main/java/com/kaba4cow/difuse/core/bean/preprocessor/support/BeanPreProcessorFactory.java
@@ -3,17 +3,23 @@ package com.kaba4cow.difuse.core.bean.preprocessor.support;
 import java.lang.reflect.Constructor;
 
 import com.kaba4cow.difuse.core.DifuseException;
+import com.kaba4cow.difuse.core.annotation.dependency.Provided;
 import com.kaba4cow.difuse.core.annotation.system.SystemBean;
 import com.kaba4cow.difuse.core.bean.preprocessor.BeanPreProcessor;
+import com.kaba4cow.difuse.core.system.bean.SystemBeanInjector;
 import com.kaba4cow.difuse.core.util.reflections.ConstructorScanner;
 
 @SystemBean
 public class BeanPreProcessorFactory {
 
+	@Provided
+	private SystemBeanInjector beanInjector;
+
 	public BeanPreProcessor createBeanPreProcessor(Class<? extends BeanPreProcessor> type) {
 		try {
 			Constructor<?> constructor = ConstructorScanner.of(type).findNoArgsConstructor();
 			Object instance = constructor.newInstance();
+			beanInjector.injectDependencies(instance);
 			return type.cast(instance);
 		} catch (Exception exception) {
 			throw new DifuseException(String.format("Could not create BeanPreProcessor of type %s", type), exception);
