8db4f4d38e8259f2aac2aca171dcdf238c48dafe
kaba4cow <kaba4cow@gmail.com>
2025-07-07 03:32:32 +0300

    Implemented BeanProtector access-check logic

--- difuse-core/src/main/java/com/kaba4cow/difuse/core/bean/protector/BeanProtector.java
+++ difuse-core/src/main/java/com/kaba4cow/difuse/core/bean/protector/BeanProtector.java
@@ -1,37 +1,60 @@
 package com.kaba4cow.difuse.core.bean.protector;
 
 import java.lang.annotation.Annotation;
-import java.lang.reflect.AnnotatedElement;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.kaba4cow.difuse.core.bean.access.provider.BeanAccessProvider;
 import com.kaba4cow.difuse.core.dependency.DependencyConsumer;
 
 public class BeanProtector {
 
-	private final Map<Class<? extends Annotation>, Annotation> accessAnnotations = new HashMap<>();
-
-	private final Map<Class<? extends Annotation>, BeanAccessProvider<?>> accessProviders = new HashMap<>();
+	private final List<AccessProviderEntry<?>> entries = new ArrayList<>();
 
 	private boolean locked = false;
 
-	BeanProtector(AnnotatedElement... elements) {}
+	BeanProtector() {}
 
 	public boolean canBeAccessedBy(DependencyConsumer consumer) {
-		return true;
+		boolean restricting = false;
+		for (AccessProviderEntry<?> entry : entries)
+			if (entry.isApplicable()) {
+				restricting = true;
+				if (entry.allowsAccess(consumer))
+					return true;
+			}
+		return !restricting;
 	}
 
-	public void addAccessProvider(Annotation annotation, BeanAccessProvider<?> accessProvider) {
+	public <T extends Annotation> void addAccessProvider(T annotation, BeanAccessProvider<T> accessProvider) {
 		if (locked)
-			throw new BeanProtectorException("Cannot add access provider. BeanProtector already locked");
-		Class<? extends Annotation> type = annotation.annotationType();
-		accessAnnotations.put(type, annotation);
-		accessProviders.put(type, accessProvider);
+			throw new BeanProtectorException("Cannot add access provider: BeanProtector already locked");
+		entries.add(new AccessProviderEntry<>(annotation, accessProvider));
 	}
 
 	public void lock() {
 		locked = true;
+	}
+
+	private static class AccessProviderEntry<T extends Annotation> {
+
+		private final T annotation;
+
+		private final BeanAccessProvider<T> accessProvider;
+
+		private AccessProviderEntry(T annotation, BeanAccessProvider<T> accessProvider) {
+			this.annotation = annotation;
+			this.accessProvider = accessProvider;
+		}
+
+		public boolean isApplicable() {
+			return accessProvider.isApplicable(annotation);
+		}
+
+		public boolean allowsAccess(DependencyConsumer consumer) {
+			return accessProvider.allowsAccess(annotation, consumer);
+		}
+
 	}
 
 }
