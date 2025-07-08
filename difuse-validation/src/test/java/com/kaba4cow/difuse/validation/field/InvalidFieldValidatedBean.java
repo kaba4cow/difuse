e161e13f749dfafbd4069653ee07ef15af001fa4
kaba4cow <kaba4cow@gmail.com>
2025-07-08 20:33:05 +0300

    Added FieldValidationTest

---
+++ difuse-validation/src/test/java/com/kaba4cow/difuse/validation/field/InvalidFieldValidatedBean.java
@@ -0,0 +1,17 @@
+package com.kaba4cow.difuse.validation.field;
+
+import javax.validation.constraints.NotNull;
+
+import com.kaba4cow.difuse.core.annotation.bean.Bean;
+import com.kaba4cow.difuse.core.annotation.bean.Lazy;
+import com.kaba4cow.difuse.validation.annotation.Validated;
+
+@Validated
+@Lazy
+@Bean
+public class InvalidFieldValidatedBean {
+
+	@NotNull
+	public final String field = null;
+
+}
