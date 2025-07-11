5359ab8082b021cd0156833733e1678cad99c7d2
kaba4cow <kaba4cow@gmail.com>
2025-07-11 13:28:19 +0300

    Added OnPropertyMissing conditiom

---
+++ difuse-conditions/src/main/java/com/kaba4cow/difuse/conditions/annotation/OnPropertyMissing.java
@@ -0,0 +1,21 @@
+package com.kaba4cow.difuse.conditions.annotation;
+
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import com.kaba4cow.difuse.conditions.condition.impl.OnPropertyMissingBeanCondition;
+
+@Documented
+@Retention(RUNTIME)
+@Target({ TYPE, METHOD })
+@Conditional(OnPropertyMissingBeanCondition.class)
+public @interface OnPropertyMissing {
+
+	String value();
+
+}
