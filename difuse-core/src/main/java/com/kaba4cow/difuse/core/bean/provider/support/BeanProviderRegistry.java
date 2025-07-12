366e97f3c8ff35b81e1bf332a7e083815588fdcb
kaba4cow <kaba4cow@gmail.com>
2025-07-12 12:26:27 +0300

    Added findBySource(BeanSource) method to BeanProviderRegistry

--- difuse-core/src/main/java/com/kaba4cow/difuse/core/bean/provider/support/BeanProviderRegistry.java
+++ difuse-core/src/main/java/com/kaba4cow/difuse/core/bean/provider/support/BeanProviderRegistry.java
@@ -3,6 +3,7 @@ package com.kaba4cow.difuse.core.bean.provider.support;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
@@ -12,41 +13,51 @@ import org.slf4j.LoggerFactory;
 
 import com.kaba4cow.difuse.core.annotation.system.SystemBean;
 import com.kaba4cow.difuse.core.bean.provider.BeanProvider;
+import com.kaba4cow.difuse.core.bean.source.BeanSource;
 
 @SystemBean
 public class BeanProviderRegistry {
 
 	private static final Logger log = LoggerFactory.getLogger("BeanProviderRegistry");
 
-	private final Map<String, Set<BeanProvider<?>>> registry = new ConcurrentHashMap<>();
+	private final Map<String, Set<BeanProvider<?>>> providersByName = new ConcurrentHashMap<>();
+
+	private final Map<BeanSource<?>, BeanProvider<?>> providersBySource = new ConcurrentHashMap<>();
 
 	public void register(BeanProvider<?> beanProvider) {
-		String name = beanProvider.getBeanSource().getInfo().getName();
-		if (registry.computeIfAbsent(name, key -> ConcurrentHashMap.newKeySet()).add(beanProvider))
+		BeanSource<?> beanSource = beanProvider.getBeanSource();
+		String name = beanSource.getInfo().getName();
+		if (providersByName.computeIfAbsent(name, key -> ConcurrentHashMap.newKeySet()).add(beanProvider)) {
+			providersBySource.put(beanSource, beanProvider);
 			log.debug("Registered {}", beanProvider);
+		}
 	}
 
 	public List<BeanProvider<?>> findByClass(Class<?> beanClass) {
-		return registry.values().stream()//
+		return providersByName.values().stream()//
 				.flatMap(Set::stream)//
 				.filter(beanProvider -> beanClass.isAssignableFrom(beanProvider.getBeanSource().getBeanClass()))//
 				.collect(Collectors.toList());
 	}
 
 	public List<BeanProvider<?>> findByNameAndClass(String name, Class<?> beanClass) {
-		if (!registry.containsKey(name))
+		if (!providersByName.containsKey(name))
 			return Collections.emptyList();
-		return registry.get(name).stream()//
+		return providersByName.get(name).stream()//
 				.filter(beanProvider -> beanClass.isAssignableFrom(beanProvider.getBeanSource().getBeanClass()))//
 				.collect(Collectors.toList());
 	}
 
 	public List<BeanProvider<?>> findEagerBeanProviders(Class<? extends BeanProvider<?>> beanProviderClass) {
-		return registry.values().stream()//
+		return providersByName.values().stream()//
 				.flatMap(Set::stream)//
 				.filter(beanProvider -> beanProvider.getBeanSource().getInfo().isEager())//
 				.filter(beanProviderClass::isInstance)//
 				.collect(Collectors.toList());
+	}
+
+	public Optional<BeanProvider<?>> findBySource(BeanSource<?> beanSource) {
+		return Optional.ofNullable(providersBySource.get(beanSource));
 	}
 
 }
